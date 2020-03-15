be58d1f359967ead139fb0cde783d1da7187aa8d
Manu Arora <mannuarora700@gmail.com>
2020-03-15 22:11:42 +0530

    Global error handling middleware

--- app.js
+++ app.js
@@ -30,12 +30,28 @@ app.use('/api/v1/users', userRouter);
 
 // THIS MIDDLEWARE IS ONLY EXECUTED BECAUSE THE ABOVE TWO COULDNT FIND ANY MATCHING ROUTES. ORDER MATTERS!
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'failed',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+  // next();
 
-  next();
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Global error handling middleware by express which automatically handles errors
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
 });
 
 module.exports = app;
