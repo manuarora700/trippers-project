798d27f534ff42398b9c829273af6460cb24c813
Manu Arora <mannuarora700@gmail.com>
2020-03-17 11:19:12 +0530

    Updated readme

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,21 +74,21 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3. Check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError('The user belonging to this JWT no longer exists', 401)
     );
   }
 
   // 4. Check if user changed password after the JWT was issued
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password, Please login again.', 401)
     );
   }
 
   // GRANT ACCESS TO PROTECTED ROUTE
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });
