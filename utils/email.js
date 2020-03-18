127ccec4079198902f938164a317cfbd0e74bbca
Manu Arora <mannuarora700@gmail.com>
2020-03-18 15:56:07 +0530

    Nodemailer gmail

--- utils/email.js
+++ utils/email.js
@@ -2,6 +2,14 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = options => {
   // 1. Create a transporter
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+    // Activate in gmail "less secure app" option
+  });
   // 2 Define email options
   // 3. Send emails with nodemailer
 };
