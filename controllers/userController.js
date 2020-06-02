64c09e7b5071f73016c4e55ddfbfcc18eaeb295e
Manu Arora <mannuarora700@gmail.com>
2020-06-02 16:57:43 +0530

    Updating user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -15,7 +23,7 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   // 1. Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -25,10 +33,24 @@ exports.updateMe = (req, res, next) => {
       )
     );
   }
-  // 2. Update user document
 
-  res.status(200).json({ status: 'success' });
-};
+  // 2. Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3. Update user document
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
