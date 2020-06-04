719b2433627013f9b39e861a25c8b5e8af615489
Manu Arora <mannuarora700@gmail.com>
2020-06-04 18:42:58 +0530

    adding cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,16 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // set secure only in production env
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
