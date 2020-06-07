a0aa945f2be2d1ce25be79abe5d792ee368df314
Manu Arora <mannuarora700@gmail.com>
2020-06-07 13:06:43 +0530

    populating data with referencing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,6 +33,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
   // Tour.findOne({ _id: req.params.id })
 
   if (!tour) {
--- models/tourModel.js
+++ models/tourModel.js
@@ -144,6 +144,15 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+
+  next();
+});
+
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took : ${Date.now() - this.start} milliseconds`);
   // console.log(docs);
