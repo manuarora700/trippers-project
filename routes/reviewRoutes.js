cbcd8143133ce1b54cfb820f562b6af618604a77
Manu Arora <mannuarora700@gmail.com>
2020-06-08 19:17:56 +0530

    Getting and creating reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(reviewController.createReview);
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
