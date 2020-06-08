a17eefbb71a3d68c659d93db9d2d9db9418d81b0
Manu Arora <mannuarora700@gmail.com>
2020-06-08 19:29:16 +0530

    Populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,18 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
