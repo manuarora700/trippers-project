3dfe063e73768a8a8b5a61b37d9172886b0ec32e
Manu Arora <mannuarora700@gmail.com>
2020-06-09 21:30:37 +0530

    /me middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,13 @@ router.patch(
   authController.updatePassword
 );
 
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
+
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
