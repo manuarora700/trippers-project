e86034ddf002550a6a0a851481feeb1d666a81bb
Manu Arora <mannuarora700@gmail.com>
2020-06-09 15:22:26 +0530

    Factory functions for deleting done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
+const factory = require('./handlerFactory');
+
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach(el => {
@@ -79,9 +81,4 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet defined'
   });
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined'
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
