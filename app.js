f32b1569b6689d5c74f2d93bd1348c6b74855e0c
Manu Arora <mannuarora700@gmail.com>
2020-06-11 02:45:58 +0530

    Starting with pug

--- app.js
+++ app.js
@@ -79,7 +79,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Paaji'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
