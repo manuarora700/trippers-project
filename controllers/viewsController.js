b798e7966fc4b45a393740f9d4f2ed83f524022c
Manu Arora <mannuarora700@gmail.com>
2020-06-11 15:38:35 +0530

    Building the tour details page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1. Get the data, for the requested tour (includes reviews and tour guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+  // 2. Build templates
+
+  // 3. Render template using data from step 1.
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 
-router.get('/tour', viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
