e8625241b276ce089e6a031ecb69cc66f7b9e6ba
Manu Arora <mannuarora700@gmail.com>
2020-06-11 20:02:56 +0530

    login form ready

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1. Get the data, for the requested tour (includes reviews and tour guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -27,3 +27,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,4 +6,6 @@ router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
 
+router.get('/login', viewsController.getLoginForm);
+
 module.exports = router;
