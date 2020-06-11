b3f28cfde1e81630b348c45b83ff65b74a93979d
Manu Arora <mannuarora700@gmail.com>
2020-06-11 14:49:53 +0530

    Overview ready // populating the data now

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //  1. Get all tour data from our backend API collection
+  const tours = await Tour.find();
+  // 2. Build template
+
+  // 3. Render the template using the tour data from the collection
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
