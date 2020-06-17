f2400baf5e52cd370fb30a90294baa04f0344f30
Manu Arora <mannuarora700@gmail.com>
2020-06-17 18:38:50 +0530

    preparing to deploy

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,3 +45,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
