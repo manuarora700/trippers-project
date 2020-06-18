d2991ebce8158cc8c993194eb220edbfc78acb01
Manu Arora <mannuarora700@gmail.com>
2020-06-19 00:43:25 +0530

    search better

--- public/js/search.js
+++ public/js/search.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const search = async searchTerm => {
-  if (!searchTerm.trim()) return;
-  window.location.href = `/search/${searchTerm}`;
+  if (!searchTerm.trim()) {
+    return location.assign('/');
+  }
+  showAlert('success', 'Searching...', 2);
+  window.setTimeout(() => {
+    location.assign(`/search/${searchTerm}`);
+  }, 200);
+  // window.location.href = `/search/${searchTerm}`;
 };
